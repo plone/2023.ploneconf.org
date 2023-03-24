ba73d1a54186e63c3675136b331d9268642f130d
Lur Ibargutxi <libargutxi@codesyntax.com>
2023-03-24 15:14:09 +0100

    prettier

--- frontend/src/addons/volto-conf2023-theme/src/components/Blocks/Teaser/TeaserMainFeatured.jsx
+++ frontend/src/addons/volto-conf2023-theme/src/components/Blocks/Teaser/TeaserMainFeatured.jsx
@@ -1,29 +1,29 @@
-import React from "react";
-import { defineMessages, useIntl, FormattedMessage } from "react-intl";
-import { Grid, Message, Icon } from "semantic-ui-react";
+import React from 'react';
+import { defineMessages, useIntl, FormattedMessage } from 'react-intl';
+import { Grid, Message, Icon } from 'semantic-ui-react';
 
-import { MaybeWrap } from "@plone/volto/components";
-import { UniversalLink } from "@plone/volto/components";
-import imageBlockSVG from "@plone/volto/components/manage/Blocks/Image/block-image.svg";
-import { getTeaserImageURL } from "@plone/volto/components/manage/Blocks/Teaser/utils";
-import { flattenToAppURL } from "@plone/volto/helpers";
-import { isInternalURL } from "@plone/volto/helpers";
-import config from "@plone/volto/registry";
+import { MaybeWrap } from '@plone/volto/components';
+import { UniversalLink } from '@plone/volto/components';
+import imageBlockSVG from '@plone/volto/components/manage/Blocks/Image/block-image.svg';
+import { getTeaserImageURL } from '@plone/volto/components/manage/Blocks/Teaser/utils';
+import { flattenToAppURL } from '@plone/volto/helpers';
+import { isInternalURL } from '@plone/volto/helpers';
+import config from '@plone/volto/registry';
 
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 const messages = defineMessages({
   PleaseChooseContent: {
-    id: "Please choose an existing content as source for this element",
+    id: 'Please choose an existing content as source for this element',
     defaultMessage:
-      "Please choose an existing content as source for this element",
+      'Please choose an existing content as source for this element',
   },
 });
-const DefaultImage = (props) => <img {...props} alt={props.alt || ""} />;
+const DefaultImage = (props) => <img {...props} alt={props.alt || ''} />;
 
 const ImageContainer = (props) => {
   const { hasImageComponent, href, defaultImageSrc } = props;
-  const Image = config.getComponent("Image").component || DefaultImage;
+  const Image = config.getComponent('Image').component || DefaultImage;
   return (
     <div className="grid-image-wrapper">
       <Image
@@ -42,7 +42,7 @@ const TeaserHomeFeatured = (props) => {
   const image = data.preview_image?.[0];
   const align = data?.styles?.align;
 
-  const hasImageComponent = config.getComponent("Image").component;
+  const hasImageComponent = config.getComponent('Image').component;
   const { openExternalLinkInNewTab } = config.settings;
   const defaultImageSrc =
     href && flattenToAppURL(getTeaserImageURL({ href, image, align }));
@@ -82,11 +82,11 @@ const TeaserHomeFeatured = (props) => {
                 <MaybeWrap
                   condition={!isEditMode}
                   as={UniversalLink}
-                  href={href["@id"]}
+                  href={href['@id']}
                   target={
                     data.openLinkInNewTab ||
-                    (openExternalLinkInNewTab && !isInternalURL(href["@id"]))
-                      ? "_blank"
+                    (openExternalLinkInNewTab && !isInternalURL(href['@id']))
+                      ? '_blank'
                       : null
                   }
                 >
